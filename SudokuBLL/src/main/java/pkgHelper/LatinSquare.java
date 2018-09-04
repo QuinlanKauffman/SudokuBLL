0a71cbf6704889c3c3fba6859452389ee1511473
qskau <qskau@128.4.142.177>
2018-09-04 17:52:30 -0400

    Class notes

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -1,8 +1,30 @@
 package pkgHelper;
 
-public class LatinSquare {
+import java.util.Arrays;
 
-	private int[][] LatinSquare
+public class LatinSquare {
+/*Rules for a Latin Square
+	-No number is repeated in any column or row
+	-Every element in the first row must match the elements in the first row
+	-Every element in the first column must match the elements in the first column
+	-For our case, every element is an int
+	-1-D arrays of ints for rows and columns
+	-Entire Latin Square is 2-D array of ints
+
+Rules for java array
+	-Index starts at 0
+	-Once array is sized, it can't be changed
+	-"int [] arr = new int[4]
+		New array length is 4
+	-Default value for int is 0
+	-"System.out.println(Arrays.toString(arr));"
+
+i++ 
+i = i + 1
+*/	
+	private int[][] LatinSquare(){
+		
+	}
 
 	public LatinSquare() {
 		
@@ -37,4 +59,13 @@ public class LatinSquare {
 		return hasDuplicates;
 	}
 	
+	int[] arr = new int[9];
+	
+	for (int i = 0; i < arr.length;i++) {
+		//Each part of for loop is optional
+		//Can declare i before the for loop
+		System.out.println(arr[i]);
+		//Scope of 'i' is just this for loop
+		//Can't call i outside the braces of this for loop
+	}
 }
