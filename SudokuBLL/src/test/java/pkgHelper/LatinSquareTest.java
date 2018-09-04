4aeeaeb766fd0495587cbcbf1f418c22e8d03583
qskau <qskau@128.4.142.177>
2018-09-04 13:38:02 -0400

    Duplicate

---
+++ SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
@@ -0,0 +1,18 @@
+package pkgHelper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LatinSquareTest {
+
+	@Test
+	public void hasDuplicates_test() {
+		
+		int [] arr = {1,2,3,4,5};
+		LatinSquare lq = new LatinSquare();
+		assertFalse(lq.hasDuplicates(arr));
+		
+	}
+
+}
