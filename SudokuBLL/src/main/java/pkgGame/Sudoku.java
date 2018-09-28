6cf1b76da29bba925e852ff06b7785dfa3229efd
qskau <qskau@128.4.142.168>
2018-09-27 22:56:13 -0400

    Stuff

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -1,8 +1,10 @@
 package pkgGame;
-import java.util.Random;
+
+import java.security.SecureRandom;
 import java.util.*;
 
 import pkgHelper.LatinSquare;
+
 public class Sudoku extends LatinSquare {
 
 		
@@ -167,10 +169,17 @@ public class Sudoku extends LatinSquare {
 		return a;
 	}
 	
-	public void createSudoku()
+	public int[][] createSudoku()
 	{
-		int[][] mySud = super.getLatinSquare();
-		int r;
+		FillDiagonalRegions();
+		System.out.println("");
+		for (int y = 0;y<iSize;y++)
+		{
+			System.out.println(Arrays.toString(super.getLatinSquare()[y]));
+		}
+		System.out.println("");
+
+		int[][] temporarySudoku;
 		
 		
 		for (int iRow = 0; iRow<iSize;iRow++)
@@ -182,43 +191,53 @@ public class Sudoku extends LatinSquare {
 				
 				//Add a condition statement to only loop at non-diagonal regions
 				//if(getRegionNmr(iCol,iRow) == )
-				r = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
+				int r = getRegionNbr(iCol, iRow);
 				
-				int i = (r/iSqrtSize);
-				int j = (r%iSqrtSize);
+				int rowIndex = (r/iSqrtSize);
+				int colIndex = (r%iSqrtSize);
 				
-				if(i != j)
-					//Assuming the diagonal regions have already been set
+				if(rowIndex != colIndex)
 				{
-					int[] c = new int[iSize];
+				
+					//Assuming the diagonal regions have already been set
+					//Change this
+				
+					int[] possibilitiesArray = new int[iSize];
 					//Creates a blank 1-D array
 					//Will fill the array with the valid values from 1 to iSize
-					int a = 0;
+					int count = 0;
 					//Keeps count of how many values are valid 
 					for (int k=1;k<=iSize;k++)
 					{
 						if(isValueValid(iCol,iRow,k) == true)
 						{
-							c[a] = k;
-							a++;
+							possibilitiesArray[count] = k;
+							count++;
 							
 						}
+						else
+							continue;
 					}
 					
-					Random rand = new Random();
-					int pickedNumber = rand.nextInt(a);
+					Random rand = new SecureRandom();
+					int pickedNumber = rand.nextInt(count);
+					System.out.println(pickedNumber);
 					//nextInt is non-inclusive
 					//Picking a random index of c
 					//Will only pick the indices that correspond to valid values
-					mySud[iRow][iCol] = c[pickedNumber];
+					temporarySudoku = super.getLatinSquare();
+					System.out.println("The possible value selected: ");
+					System.out.println(possibilitiesArray[pickedNumber]);
+					temporarySudoku[iRow][iCol] = possibilitiesArray[pickedNumber];
+					super.setLatinSquare(temporarySudoku);
 				}
 			}
 		}
 		
-		super.setLatinSquare(mySud);
+		return (super.getLatinSquare());
 	}
 	
-	private int getRegionNbr(int iCol, int iRow)
+	public int getRegionNbr(int iCol, int iRow)
 	{
 		int r = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
 		return r;
@@ -226,29 +245,36 @@ public class Sudoku extends LatinSquare {
 	
 	private void setRegion(int r)
 	{
-		int [][] mySud = super.getLatinSquare();
-		int[] a = getRegion(r);
+		
+		
+		int[][] temporarySudoku;
+		//int[] a = getRegion(r);
+		int[] a = new int[iSize];
 		for (int k=0;k<a.length;k++)
 		{
 			a[k] = k+1;
 		}
 		
 		int count = 0;
-		int j = (r % iSqrtSize) * iSqrtSize;
+		
 		int i = (r / iSqrtSize) * iSqrtSize;
-		int jMax = j + iSqrtSize;
+		int j = (r % iSqrtSize) * iSqrtSize;
 		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+
 		for (;i<iMax;i++)
 		{
 			for (;j<jMax;j++)
 			{
-				mySud[i][j] = a[count];
+				
+				temporarySudoku = super.getLatinSquare();
+				temporarySudoku[i][j] = a[count];
+				super.setLatinSquare(temporarySudoku);
 				count++;
 			}
 		}
 		
-		super.setLatinSquare(mySud);
-		//Setting the latin square for a given region
+		
 	}
 	
 	private void FillDiagonalRegions()
@@ -257,31 +283,31 @@ public class Sudoku extends LatinSquare {
 		
 		//Need to create formula that checks if a region is on the diagonal
 		
-		for(int r=0;r<iSize;r++)
+		for(int r=0;r<iSqrtSize;r++)
 		{
-			//Loop over the regionNumbers
-			int indexRow = (r/iSqrtSize);
-			int indexCol = (r%iSqrtSize);
+			if (r == 0)
+			{
+				setRegion(r);
+			}
+			else
+			{
+				setRegion(r*iSqrtSize+1);
+			}
 			
-			if(indexRow == indexCol)
-				//Need a condition statement to check if region is on the diagonal
-				{
-					setRegion(r);
-					//SetRegion should shuffle the array inside the region
-				}
 		}
-		
 	}
 	
 	private void shuffleArray(int[] ar)
 	{
 		
+		int pickedIndex;
 		int holder1;
 		int holder2;
 		for (int i=0;i<ar.length;i++)
+			
 		{
-			Random rand = new Random();
-			int pickedIndex = rand.nextInt(ar.length-i);
+			Random rand = new SecureRandom();
+			pickedIndex = rand.nextInt(ar.length-i);
 			pickedIndex = pickedIndex - i;
 			//Picking a random index from current i to length
 			
@@ -303,9 +329,29 @@ public class Sudoku extends LatinSquare {
 	
 	private void shuffleRegion(int r)
 	{
+		int[][] temporarySudoku;
+		
 		int[] ar = getRegion(r);
 		shuffleArray(ar);
-	
+		
+		//int[][] mySud = super.getLatinSquare();
+		//Setting the region
+		int count = 0;
+		int i = (r / iSqrtSize) * iSqrtSize;
+		int j = (r % iSqrtSize) * iSqrtSize;
+		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+
+		for (;i<iMax;i++)
+		{
+			for (;j<jMax;j++)
+			{
+				temporarySudoku = super.getLatinSquare();
+				temporarySudoku[i][j] = ar[count];
+				super.setLatinSquare(temporarySudoku);
+				count++;
+			}
+		}
 		
 		
 	}
